23e9d21063f839034fc863fd9ece609d7657bc9f
Kouassi Kevin Kouadio <kevkouadio@gmil.com>
2020-11-06 14:29:29 -0500

    add of components

---
+++ src/components/Header/index.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "./style.css";
+
+function Footer() {
+  return (
+    <header className="header">
+      <h1>EMPLOYEES DIRECTORY</h1>
+    </header>
+  );
+}
+
+export default Footer;
